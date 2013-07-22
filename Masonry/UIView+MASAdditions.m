cd9ac18169900c83d09ee67a0be50d5a143b6196
Jonas Budelmann <jonas.budelmann@gmail.com>
2013-07-22 18:38:48 +1000

    fixing #define MAS_SHORTHAND

--- Masonry/UIView+MASAdditions.m
+++ Masonry/UIView+MASAdditions.m
@@ -64,28 +64,3 @@
 }
 
 @end
-
-#define MAS_ATTR_FORWARD(attr)  \
-- (MASViewAttribute *)attr {    \
-    return [self mas_##attr];   \
-}
-
-@implementation UIView (MASShortHandAdditions)
-
-MAS_ATTR_FORWARD(top);
-MAS_ATTR_FORWARD(left);
-MAS_ATTR_FORWARD(bottom);
-MAS_ATTR_FORWARD(right);
-MAS_ATTR_FORWARD(leading);
-MAS_ATTR_FORWARD(trailing);
-MAS_ATTR_FORWARD(width);
-MAS_ATTR_FORWARD(height);
-MAS_ATTR_FORWARD(centerX);
-MAS_ATTR_FORWARD(centerY);
-MAS_ATTR_FORWARD(baseline);
-
-- (void)buildConstraints:(void(^)(MASConstraintBuilder *constraints))block {
-    [self mas_buildConstraints:block];
-}
-
-@end
