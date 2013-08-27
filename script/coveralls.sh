f6be2a3b74d78a9f96402002b1d1ef274c6b2489
Jonas Budelmann <jonas.budelmann@gmail.com>
2013-08-28 09:40:59 +1000

    adding coveralls

---
+++ script/coveralls.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+source env.sh
+declare -r gcov_dir="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
+
+## ======
+
+generateGcov()
+{
+	#  doesn't set output dir to gcov...
+	cd "${gcov_dir}"
+	for file in ${gcov_dir}/*.gcda
+	do
+		gcov-4.2 "${file}" -o "${gcov_dir}"
+	done
+	cd -
+}
+
+copyGcovToProjectDir()
+{
+	cp -r "${gcov_dir}" gcov
+}
+
+removeGcov()
+{
+	rm -r gcov
+}
+
+main()
+{
+# generate + copy
+	generateGcov
+	copyGcovToProjectDir
+# post
+	coveralls ${@+"$@"}
+# clean up
+	removeGcov
+}
+
+main ${@+"$@"}
---
+++ script/exportenv.sh
@@ -0,0 +1,1 @@
+export | egrep '( BUILT_PRODUCTS_DIR)|(CURRENT_ARCH)|(OBJECT_FILE_DIR_normal)|(SRCROOT)|(OBJROOT)' > script/env.sh
