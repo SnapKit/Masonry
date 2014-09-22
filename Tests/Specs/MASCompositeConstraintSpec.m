838cea80161f91b9742c6d329087f04ddd81ca74
Pavel Mazurin <pavel.mazurin@tomtom.com>
2014-09-22 15:09:16 +0200

    Added tests for composite constraint deactivation

--- Tests/Specs/MASCompositeConstraintSpec.m
+++ Tests/Specs/MASCompositeConstraintSpec.m
@@ -152,6 +152,27 @@ SpecBegin(MASCompositeConstraint) {
     expect(superview.constraints).to.haveCountOf(0);
 }
 
+#if __IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_8_0
+- (void)testDeactivate
+{
+    NSArray *children = @[
+        [[MASViewConstraint alloc] initWithFirstViewAttribute:view.mas_leading],
+        [[MASViewConstraint alloc] initWithFirstViewAttribute:view.mas_trailing]
+    ];
+    composite = [[MASCompositeConstraint alloc] initWithChildren:children];
+    composite.delegate = delegate;
+    MAS_VIEW *newView = MAS_VIEW.new;
+    [superview addSubview:newView];
+    
+    //first equality statement
+    composite.equalTo(newView);
+    [composite install];
+    
+    expect(superview.constraints).to.haveCountOf(2);
+    [composite deactivate];
+    expect(superview.constraints).to.haveCountOf(0);}
+#endif
+
 
 - (void)testAttributeChainingShouldCallDelegate {
     NSArray *children = @[
