4259cdbb8f8c3200820295b3c306bec2f48b3d47
Michael Koukoullis <mike@kouky.org>
2015-01-19 19:00:41 +1100

    MASExampleAspectFitView class

---
+++ Examples/Masonry iOS Examples/MASExampleAspectFitView.m
@@ -0,0 +1,21 @@
+//
+//  MASExampleAspectFitView.m
+//  Masonry iOS Examples
+//
+//  Created by Michael Koukoullis on 19/01/2015.
+//  Copyright (c) 2015 Jonas Budelmann. All rights reserved.
+//
+
+#import "MASExampleAspectFitView.h"
+
+@implementation MASExampleAspectFitView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
