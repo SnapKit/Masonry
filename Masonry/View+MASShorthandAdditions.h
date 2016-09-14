549155691b279d19375a0aedd8d459c79ccb3d2c
Patrick Pöchhacker <patrick.poechhacker@willhaben.at>
2016-09-14 09:55:16 +0200

    removed superfluous NS_NOESCAPE

--- Masonry/View+MASShorthandAdditions.h
+++ Masonry/View+MASShorthandAdditions.h
@@ -94,7 +94,7 @@ MAS_ATTR_FORWARD(centerYWithinMargins);
 
 #endif
 
-- (MASViewAttribute *(NS_NOESCAPE ^)(NSLayoutAttribute))attribute {
+- (MASViewAttribute *(^)(NSLayoutAttribute))attribute {
     return [self mas_attribute];
 }
 
