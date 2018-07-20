e1a392a70066fb6d39d305c98df5ee11815dbafb
xiaocao <B12050217@163.com>
2018-07-20 17:23:02 +0800

    change the priorityLow() to priorityHigh()

--- Examples/Masonry iOS Examples/MASExampleUpdateView.m
+++ Examples/Masonry iOS Examples/MASExampleUpdateView.m
@@ -44,8 +44,8 @@
 
     [self.growingButton updateConstraints:^(MASConstraintMaker *make) {
         make.center.equalTo(self);
-        make.width.equalTo(@(self.buttonSize.width)).priorityLow();
-        make.height.equalTo(@(self.buttonSize.height)).priorityLow();
+        make.width.equalTo(@(self.buttonSize.width)).priorityHigh();
+        make.height.equalTo(@(self.buttonSize.height)).priorityHigh();
         make.width.lessThanOrEqualTo(self);
         make.height.lessThanOrEqualTo(self);
     }];
